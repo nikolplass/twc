f5fe38cad22fdee2b86e5ff0b00760dbc1cec39e
Nikol Plass <nikolplass@hotmail.com>
2018-01-02 17:08:59 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function initMap() {
+// Create a map object and specify the DOM element for display.
+var map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: 37.8744733, lng: -122.4604833},
+	zoom: 8
+	});
+}
+
+
